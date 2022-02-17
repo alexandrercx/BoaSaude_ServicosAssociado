c2d68cf28c6ed795f397c241ea2875219921ea1f
Jhonathan Amazonas Ferreira <jhonathan.ferreira@getnet.com.br>
2022-02-17 20:46:30 -0300

    Criação do contexto de banco de dados e entidades

--- Infrastructure/Context/Contexto.cs
+++ Infrastructure/Context/Contexto.cs
@@ -1,18 +1,50 @@
-﻿using Domain.Models;
+﻿using Infrastructure.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.Context
 {
-    public class Contexto : DbContext
-    {
-        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+   public class Contexto : DbContext
+   {
+      public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
+      public DbSet<Agendamento> Agendamento { get; set; }
+      public DbSet<Associado> Associado { get; set; }
+      public DbSet<Conveniado> Conveniado { get; set; }
+      public DbSet<Endereco> Endereco { get; set; }
 
-        public DbSet<ServicosAssociado> ServicosAssociados { get; set; }
+      protected override void OnModelCreating(ModelBuilder modelBuilder)
+      {
+         modelBuilder.Entity<Agendamento>()
+           .HasKey(p => new { p.Seql_Agendamento });
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
-    }
+         modelBuilder.Entity<Agendamento>()
+            .Property(r => r.Seql_Agendamento)
+            .ValueGeneratedOnAdd();
+
+         modelBuilder.Entity<Associado>()
+            .HasKey(p => new { p.Seql_Associado });
+
+         modelBuilder.Entity<Associado>()
+            .Property(r => r.Seql_Associado)
+            .ValueGeneratedOnAdd();
+
+         modelBuilder.Entity<Conveniado>()
+            .HasKey(p => new { p.Seql_Conveniado });
+
+         modelBuilder.Entity<Conveniado>()
+            .Property(r => r.Seql_Conveniado)
+            .ValueGeneratedOnAdd();
+
+         modelBuilder.Entity<Endereco>()
+            .HasKey(p => new { p.Seql_Endereco });
+
+         modelBuilder.Entity<Endereco>()
+            .Property(r => r.Seql_Endereco)
+            .ValueGeneratedOnAdd();
+
+         base.OnModelCreating(modelBuilder);
+      }
+   }
 }
---
+++ Infrastructure/Entities/Agendamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Infrastructure.Entities
+{
+   [Table("Agendamento", Schema ="dbo")]
+   public class Agendamento
+   {
+      public int Seql_Agendamento { get; set; }
+      public TipoAtendimento Cod_TipoAtendimento { get; set; }
+      public int Seql_Associado { get; set; }
+      public Associado Associado { get; set; }
+      public int Seql_Conveniado { get; set; }
+      public Conveniado Conveniado { get; set; }
+      public int Seql_Endereco { get; set; }
+      public Endereco Endereco { get; set; }
+      public DateTime Inst_Atendimento { get; set; }
+      public DateTime Inst_Agendamento { get; set; }
+
+   }
+}
